19c5e1e51bfacd2bd90f802f4ec95a5573b5fe5e
Harshaan Nihal <harshaan.n.khan@gmail.com>
2021-11-16 14:31:02 +0530

    add port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const commonRouter = require("./routes/commonRoute");
 const postRouter = require("./routes/postRoute");
 const messageRouter = require("./routes/messageRoute");
 const app = express();
-const port = "5000";
+const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
